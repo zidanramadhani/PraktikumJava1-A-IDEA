67f080641a65cc68f9973745ff2e5f72e9424799
zidanoke <zidanramadhani007@gmail.com>
2021-06-13 00:06:01 +0800

    Method hitung usia

--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Mahasiswa {
@@ -18,5 +19,22 @@ public class Mahasiswa {
     }
     void menyapa(){
         System.out.println("Hai nama aku : " + this.nama);
+    }
+    int hitungUsia(){
+        Calendar tanggalLahir = Calendar.getInstance();
+        tanggalLahir.setTime(this.tanggalLahir);
+        Calendar hariIni = Calendar.getInstance();
+
+        int selisihTahun = hariIni.get(Calendar.YEAR) - tanggalLahir.get(Calendar.YEAR);
+
+        if (hariIni.get(Calendar.MONTH) < tanggalLahir.get(Calendar.MONTH) ){
+            selisihTahun--;
+        }else{
+            if (hariIni.get(Calendar.MONTH) == tanggalLahir.get(Calendar.MONTH)
+            && hariIni.get(Calendar.DAY_OF_MONTH) < tanggalLahir.get(Calendar.DAY_OF_MONTH)){
+                selisihTahun--;
+            }
+        }
+        return selisihTahun;
     }
 }
--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -21,6 +21,7 @@ public class MahasiswaBeraksi {
 
         mahasiswa.tampilkanAtribut();
         mahasiswa.menyapa();
+        System.out.println("Usiaku : " + mahasiswa.hitungUsia() + " tahun");
 
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19630500";
