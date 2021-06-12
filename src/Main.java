e300617124376592fe3b7fbe687b714330a241c9
zidanoke <zidanramadhani007@gmail.com>
2021-06-12 22:24:19 +0800

    Operasi Tipe Data String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         String namaDepan = "Muhammad";
@@ -16,5 +18,21 @@ public class Main {
         String uniskaString = new String(uniskaChar);
         System.out.println(uniskaChar);
         System.out.println(uniskaString);
+
+        String namaLengkap = namaDepan + " " + namaTengah + " " + namaBelakang;
+
+        System.out.println(namaLengkap.length());
+        System.out.println(namaLengkap.indexOf("dan"));
+        System.out.println(namaLengkap.substring(8));
+        System.out.println(namaLengkap.substring(8,13));
+        System.out.println(namaLengkap.replace("Zid", "Ri"));
+        System.out.println(namaLengkap.toLowerCase());
+        System.out.println(namaLengkap.toUpperCase());
+        System.out.println(namaLengkap.charAt(9));
+
+        String[] namaArray = namaLengkap.split(" ");
+
+        for (String nama: namaArray)
+            System.out.println(nama);
     }
 }
