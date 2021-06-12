de8792f5218e2afe5872cecb88c12a089ab6905c
zidanoke <zidanramadhani007@gmail.com>
2021-06-12 21:47:20 +0800

    Membuat Main Class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Muhammad";
+        String namaTengah = "Zidan";
+        String namaBelakang = "Ramadhani";
+
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan+namaTengah+namaBelakang);
+    }
+}
